838c40814324da943f59e521aedcc9e7bbdaa3b3
LeoMdrs <leandro.mdrs06@gmail.com>
2021-10-06 09:17:45 -0300

    Create "Layout" and "Title" components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+
+      <Layout title="Simple crud app">
+        <span>Content</span>
+      </Layout>
+
     </div>
   )
 }
