1eaf46d382e54d8b474058274fa085922fe570ad
LeoMdrs <leandro.mdrs06@gmail.com>
2021-10-07 22:22:25 -0300

    Create 'index' page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,62 @@
+import { useEffect, useState } from "react";
+import ClientCollection from "../backend/db/ClientCollection";
+import Button from "../components/Button";
+import Form from "../components/Form";
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
+import ClientRepository from "../core/ClientRepository";
+import useClients from "../hooks/useClients";
 
 export default function Home() {
+
+  const {
+    tableIsVisible,
+    client,
+    clients,
+    newClient,
+    saveClient,
+    deletedClient,
+    selectedClient,
+    getAllClients,
+    showTable
+  } = useClients()
+
   return (
     <div className={`
-      flex justify-center items-center h-screen
+      flex h-screen justify-center items-center
       bg-gradient-to-r from-blue-500 to-purple-500
       text-white
     `}>
 
       <Layout title="Simple crud app">
-        <span>Content</span>
+        {tableIsVisible ? (
+          <>
+            <div className="flex justify-end">
+              <Button
+                color="green"
+                className="mb-4"
+                onClick={newClient}
+              >
+                New Client
+              </Button>
+            </div>
+
+            <Table
+              clients={clients}
+              // Submit functions via direct communication to this component
+              selectedClient={selectedClient}
+              deletedClient={deletedClient}
+            />
+          </>
+
+        ) : (
+          <Form
+            client={client}
+            canceled={() => showTable}
+            changedClient={saveClient}
+          />
+        )}
       </Layout>
 
     </div>
